ec76829b050313951e1d018981e346cba840aae4
“Anjali <anjali.cspatidar@gmail.com>
2025-10-18 20:34:14 -0400

    render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+/* eslint-env node */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['anjali-c-patidar.onrender.com'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
 })
